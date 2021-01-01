0246196d575a2fcd720b30b8d0c9f5db5c0df230
dllgdxlgy <1069098299@qq.com>
2021-01-01 15:05:25 +0800

    跟着狂神说学习的mybatis，第一次提交，这是初次搭建mybatis项目。

---
+++ mybatis-01/src/main/java/com/lgy/dao/userDao.java
@@ -0,0 +1,10 @@
+package com.lgy.dao;
+
+import com.lgy.pojo.User;
+
+import java.util.List;
+
+public interface userDao {
+
+    public List<User> getUserList();
+}
---
+++ mybatis-01/src/test/java/com/lgy/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package com.lgy.dao;
+
+import com.lgy.pojo.User;
+import com.lgy.utils.mybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+
+    @Test
+    public void test(){
+
+        //获取sqlsession对象
+        SqlSession sqlSession = mybatisUtils.getSqlSession();
+        //执行SQl
+        userDao userDao = sqlSession.getMapper(userDao.class);
+        List<User> userList = userDao.getUserList();
+        for (User user:userList){
+            System.out.println(user);
+        }
+
+        //关闭SqlSession
+        sqlSession.close();
+    }
+}
