fe034470aec4a1bea7764c6b3b744752f0f7d29d
dllgdxlgy <1069098299@qq.com>
2021-01-11 21:54:47 +0800

    微改

--- mybatis-02/src/test/java/com/lgy/dao/UserDaoTest.java
+++ mybatis-02/src/test/java/com/lgy/dao/UserDaoTest.java
@@ -25,4 +25,41 @@ public class UserDaoTest {
         sqlSession.close();
     }
 
+
+//    测试使用假名是不是成功
+    @Test
+    public void test2(){
+
+        //获取sqlsession对象
+        SqlSession sqlSession = mybatisUtils.getSqlSession();
+        //执行SQl
+        userDao userDao = sqlSession.getMapper(userDao.class);
+        User user = userDao.getUserById(1);
+
+        System.out.println(user.toString());
+
+
+        //关闭SqlSession
+        sqlSession.close();
+    }
+
+    @Test
+    public void test3(){
+        SqlSession sqlSession = mybatisUtils.getSqlSession();
+        userDao mapper = sqlSession.getMapper(userDao.class);
+
+        int i = mapper.addUser(new User(7, "lgyss", "123"));
+        if (i<0){
+            System.out.println("插入失败");
+
+        }else {
+            System.out.println("插入成功");
+        }
+
+
+        sqlSession.commit();
+        sqlSession.close();
+
+    }
+
 }
